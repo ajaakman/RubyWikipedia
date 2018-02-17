f74bc6b2c96fcc293e595ca6a0f2aa84817b31cf
ajaakman <jaakmanartur@gmail.com>
2018-02-17 22:45:52 +0000

    Added CSS and ActiveRecords DB

--- wiki.rb
+++ wiki.rb
@@ -1,6 +1,17 @@
 # To run code in browser write "localhost:4567/".
 
 require 'sinatra' # ctrl+c to terminate the server.
+require 'sinatra/activerecord'
+
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3',
+  :database => 'wiki.db'
+) 
+
+class User < ActiveRecord::Base
+validates :username, presence: true, uniqueness: true
+validates :password, presence: true
+end
 
 $myinfo = "Artur Jaakman" # $ indicates global variable.
 
@@ -47,6 +58,50 @@ get '/' do # Root directory of web server.
 	
 	erb :home # Calls home view.
 	
+end
+
+get '/login' do
+   erb :login 
+end
+
+post '/login' do
+   $credentials = [params[:username],params[:password]]
+   @Users = User.where(:username => $credentials[0]).to_a.first
+   if @Users
+		if @Users.password == $credentials[1]
+			redirect '/'
+		else
+		$credentials = ['','']
+		redirect '/wrongaccount'
+	end
+   else
+	$credentials = ['','']
+	redirect '/wrongaccount'
+   end
+end
+
+get '/createaccount' do
+   erb :createaccount
+end
+
+post '/createaccount' do
+   n = User.new   
+   n.username = params[:username]
+   n.password = params[:password]    
+   if n.username == "Admin" and n.password == "Password"
+		n.edit = true 
+end
+   n.save    
+   redirect "/"
+end
+
+get '/logout' do
+   $credentials = ['','']
+   redirect '/'
+end
+
+get '/wrongaccount' do
+   erb :wrongaccount
 end
 
 get '/about' do # About page.
