08e60649139446317ffc3d41270ee9e22c178bf0
ajaakman <jaakmanartur@gmail.com>
2018-02-18 16:15:24 +0000

    Added password validation using JS

---
+++ public/script.js
@@ -0,0 +1,15 @@
+
+function validateForm() {
+
+    var x = document.forms["myForm"]["username"].value;
+    var y = document.forms["myForm"]["password"].value;
+
+    if (x == null || x == "" || y == null || y == "") {
+
+        alert("Invalid Username or Password.");
+
+        return false;
+
+    }
+
+} 
--- wiki.rb
+++ wiki.rb
@@ -13,9 +13,9 @@ ActiveRecord::Base.establish_connection(
 
 class User < ActiveRecord::Base
 
-validates :username, presence: true, uniqueness: true
-
-validates :password, presence: true
+  validates :username, presence: true, uniqueness: true
+  
+  validates :password, presence: true
 
 end
 
